d859f5ad0f2ef6bb9ccb3cb539ba77d7d68cf6de
Daniel Sousa <1241584@isep.ipp.pt>
2025-03-06 16:59:54 +0000

    View dos produtos

---
+++ src/View/ProdutoView.java
@@ -0,0 +1,40 @@
+package View;
+
+import Utils.Tools;
+public class ProdutoView {
+
+
+    public void exibirProduto() {
+        int opcao;
+
+        do {
+            System.out.println("\nMenu:");
+            System.out.println("1. Criar Produto");
+            System.out.println("2. Editar Produto");
+            System.out.println("3. Apagar Produto");
+            System.out.println("4. Listar Produtos");
+            System.out.println("0. Voltar ao menu principal...");
+            System.out.print("Escolha uma opção: ");
+            opcao = Tools.scanner.nextInt();
+            Tools.scanner.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente...");
+            }
+        } while (opcao != 0);
+    }
+
+
+}
