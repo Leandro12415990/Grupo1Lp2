c93ea3c5f2325a53bd8327bca662c5d77ef8c2ae
Leandro Monteiro <1241590@isep.ipp.pt>
2025-03-11 22:33:55 +0000

    commit

--- src/Utils/Tools.java
+++ src/Utils/Tools.java
@@ -5,33 +5,8 @@ import Model.Utilizador;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 public class Tools {
     public static Scanner scanner = new Scanner(System.in);
-
-    public static String separador(){
-        return ";";
-    }
-
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-    public static LocalDate parseDate(String dateStr) {
-        if (dateStr == null || dateStr.isEmpty()) {
-            return null;
-        }
-        try {
-            return LocalDate.parse(dateStr, FORMATTER);
-        } catch (DateTimeParseException e) {
-            System.err.println("Data inválida: " + dateStr);
-            return null;
-        }
-    }
-
-    public static String formatDate(LocalDate date) {
-        return (date != null) ? date.format(FORMATTER) : "";
-    }
     public static List<Utilizador> utilizadores = new ArrayList<>();
 }
