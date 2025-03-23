f8e47d8e6d59254dd6b08236cd567313c1b51e5c
Leandro Monteiro <1241590@isep.ipp.pt>
2025-03-23 13:04:58 +0000

    Login Final Commit

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -7,6 +7,9 @@ import Utils.Tools;
 public class LoginController {
     public static boolean verificarLogin(String email, String password)
     {
+        boolean respLerDados = lerDados();
+        if (!respLerDados) return false;
+
         for (Utilizador u : Tools.utilizadores)
         {
             if (email.equals(u.getEmail()) && password.equals(u.getPassword()))
@@ -17,5 +20,12 @@ public class LoginController {
         }
 
         return false;
+    }
+
+    public static boolean lerDados()
+    {
+        boolean respLerDados = LoginUtilizadorBll.lerDados();
+        if (respLerDados) return true;
+        else return false;
     }
 }
