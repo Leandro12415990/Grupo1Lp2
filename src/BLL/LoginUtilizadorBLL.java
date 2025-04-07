dfc51ac91333dbd14df43ca89cb0896887e4e70f
Oliveira4797 <1241581@isep.ipp.pt>
2025-04-07 18:28:39 +0100

    1

--- src/BLL/LoginUtilizadorBLL.java
+++ src/BLL/LoginUtilizadorBLL.java
@@ -1,37 +1,20 @@
 package BLL;
 
-import DAL.ImportDal;
 import Model.Utilizador;
 import Utils.Tools;
 import Model.ClienteSessao;
 
-import java.time.LocalDate;
-
 public class LoginUtilizadorBLL {
-    public static Utilizador login(String email, String password)
-    {
-        for (Utilizador u : Tools.utilizadores)
-        {
-
-            if (email.equalsIgnoreCase(u.getEmail()) && password.equals(u.getPassword()))
-                u.setUltimoLogin(LocalDate.now());
-                ImportDal.gravarUtilizador(Tools.utilizadores);
-
-                if (u.getTipoUtilizador() == Tools.tipoUtilizador.GESTOR.getCodigo() || u.getTipoUtilizador() == Tools.tipoUtilizador.CLIENTE.getCodigo()) return u;
-
-
-                ClienteSessao.setIdCliente(u.getId());
-                if (u.getTipoUtilizador() == 1) return 1;  // Administrador
-                else if (u.getTipoUtilizador() == 2) return 2;  // Cliente
-
-            }
+    public static Utilizador login(String email, String password) {
+        for (Utilizador u : Tools.utilizadores) {
+            if (u.getTipoUtilizador() == Tools.tipoUtilizador.GESTOR.getCodigo() ||
+                    u.getTipoUtilizador() == Tools.tipoUtilizador.CLIENTE.getCodigo())
+                return u;
+            ClienteSessao.setIdCliente(u.getId());
         }
-        return null;
+            return null;
+        }
+
     }
 
-    public static boolean lerDados() {
-        Tools.utilizadores = ImportDal.carregarUtilizador();
-        return Tools.utilizadores != null;
-    }
-}
 
