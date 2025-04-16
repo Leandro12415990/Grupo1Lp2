73cff7bad778c9a7f92dbf81274e9c45ea7ce3cd
Pedro Pereira <1241594@isep.ipp.pt>
2025-04-16 15:31:37 +0100

    [Menus] Retirar Static e usar instâncias

--- src/View/MenuInicialView.java
+++ src/View/MenuInicialView.java
@@ -3,11 +3,11 @@ package View;
 import Model.Utilizador;
 import Utils.Tools;
 
-
 import static Utils.Tools.scanner;
 
 public class MenuInicialView {
-    public static void menuInicial() {
+
+    public void menuInicial(LoginView loginView,MenuGestorView menuGestorView, MenuClienteView menuClienteView,UtilizadorView utilizadorView, LeilaoView leilaoView, ProdutoView produtoView, EstatisticaView estatisticaView, TransacaoView transacaoView, LanceView lanceView) {
         int opcao;
         do {
             System.out.println("\nBem-vindo à leiloeira Valor em Alta!\n");
@@ -17,24 +17,25 @@ public class MenuInicialView {
             System.out.println("0. Sair...");
 
             opcao = Tools.pedirOpcaoMenu("Escolha uma opção: ");
-
             scanner.nextLine().trim();
 
             switch (opcao) {
                 case 1:
-                    Utilizador utilizador = LoginView.login();
+                    Utilizador utilizador = loginView.login();
+
                     if (utilizador == null) {
                         System.out.println("Erro a fazer Login");
                     } else if (utilizador.getTipoUtilizador() == Tools.tipoUtilizador.GESTOR.getCodigo()) {
-                        MenuGestorView.exibirMenu();
+                        menuGestorView.exibirMenu(utilizadorView, leilaoView, produtoView, estatisticaView, transacaoView);
                     } else if (utilizador.getTipoUtilizador() == Tools.tipoUtilizador.CLIENTE.getCodigo()) {
-                        MenuClienteView.exibirMenu(utilizador);
+                        menuClienteView.exibirMenu(utilizador,utilizadorView,transacaoView,lanceView);
                     }
                     break;
                 case 2:
-                    UtilizadorView.registarCliente();
+                    utilizadorView.registarCliente();
                     break;
                 case 0:
+                    // Fecha o scanner e termina o sistema
                     System.out.println("A desligar sistema...");
                     scanner.close();
                     return;
