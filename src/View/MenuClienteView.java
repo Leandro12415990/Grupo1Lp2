4e0c06847c6b66e80dfb1dffda6a6808b99a4620
Oliveira4797 <1241581@isep.ipp.pt>
2025-04-18 14:29:10 +0100

    [UTILIZADOR]--> Implementado opçao de cliente ver os seus proprios dados

--- src/View/MenuClienteView.java
+++ src/View/MenuClienteView.java
@@ -10,8 +10,9 @@ public class MenuClienteView {
         while (true) {
             System.out.println("\n" + "=".repeat(5) + " MENU CLIENTE " + "=".repeat(5));
             System.out.println("1. Editar Ficha de Cliente");
-            System.out.println("2. Carteira");
-            System.out.println("3. Menu Lances");
+            System.out.println("2. Ver os Meus Dados");
+            System.out.println("3. Carteira");
+            System.out.println("4. Menu Lances");
             System.out.println("0. Sair...");
             System.out.print("Escolha uma opção: ");
             System.out.println(utilizador.getId());
@@ -24,16 +25,17 @@ public class MenuClienteView {
                     utilizadorView.editarCliente(utilizador);
                     break;
                 case 2:
+                    utilizadorView.verDadosCliente(utilizador);
+                case 3:
                     transacaoView.exibirMenuTransacao();
                     break;
-                case 3:
+                case 4:
                     lanceView.exibirMenuLance();
                     break;
                 case 0:
                     System.out.println("A sair...");
-                    // Chama o logout para limpar o id do cliente
-                    clienteSessao.logout();  // Limpa o ID do cliente e efetua o logout
-                    return;  // Sai do loop e encerra o menu
+                    clienteSessao.logout();
+                    return;
                 default:
                     System.out.println("Opção inválida, tenta novamente.");
             }
--- src/View/UtilizadorView.java
+++ src/View/UtilizadorView.java
@@ -186,4 +186,26 @@ public class UtilizadorView {
                     utilizador.getSaldo());
         }
     }
+
+    public void verDadosCliente(Utilizador utilizador) {
+        System.out.println("\n" + "=".repeat(10) + " OS SEUS DADOS " + "=".repeat(10));
+
+        String estadoStr = Tools.estadoUtilizador.fromCodigo(utilizador.getEstado()).name();
+        String tipoUtilizadorStr = Tools.tipoUtilizador.fromCodigo(utilizador.getTipoUtilizador()).name();
+
+        System.out.println("ID: " + utilizador.getId());
+        System.out.println("Nome: " + utilizador.getNomeUtilizador());
+        System.out.println("Email: " + utilizador.getEmail());
+        System.out.println("Data de Nascimento: " + Tools.formatDate(utilizador.getDataNascimento()));
+        System.out.println("Morada: " + utilizador.getMorada());
+        System.out.println("Data de Registo: " + Tools.formatDate(utilizador.getDataRegisto()));
+        System.out.println("Último Login: " + Tools.formatDate(utilizador.getUltimoLogin()));
+        System.out.println("Tipo de Utilizador: " + tipoUtilizadorStr);
+        System.out.println("Estado: " + estadoStr);
+        System.out.printf("Saldo atual: %.2f€\n", utilizador.getSaldo());
+
+        System.out.println("=".repeat(35));
+    }
+
+
 }
